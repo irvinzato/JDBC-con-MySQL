56180150c29ad557e07e1e122e40c5f5c30dd88a
irvinzato <multizato@hotmail.com>
2022-08-23 11:21:43 -0500

    Crear, actualizar y borrar implementados

--- src/main/java/org/rivera/java/jdbc/repositorio/ProductoRepositorioImpl.java
+++ src/main/java/org/rivera/java/jdbc/repositorio/ProductoRepositorioImpl.java
@@ -42,24 +42,46 @@ public class ProductoRepositorioImpl implements Repositorio<Producto>{
     Producto product = null;
     try(PreparedStatement stmt = getConnection().prepareStatement("SELECT * FROM productos WHERE id = ?")) { //PreparedStatement porque pueden variar los parámetros de la consulta
       stmt.setLong(1, id); //Asigno parámetro del ID
-      ResultSet rs = stmt.executeQuery();
-      if( rs.next() ) {
-        product = createProduct( rs );
+      try (ResultSet rs = stmt.executeQuery()) {  //Para tener el auto close
+        if (rs.next()) {
+          product = createProduct(rs);
+        }
       }
-      rs.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
     return product;
   }
 
-  @Override
+  @Override //Ocupo el método guardar para crear y actualizar
   public void save(Producto producto) {
-
+    String query;
+    if ( producto.getId() != null && producto.getId() > 0 ) {
+      query = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+    } else {
+      query = "INSERT INTO productos(nombre, precio, fecha_registro) VALUES(?, ?, ?)";
+    }
+    try( PreparedStatement stmt = getConnection().prepareStatement(query) ) {
+      stmt.setString(1, producto.getName());
+      stmt.setLong(2, producto.getPrice());
+      if ( producto.getId() != null && producto.getId() > 0 ) {
+        stmt.setLong(3, producto.getId());
+      } else {
+        stmt.setDate(3, new Date(producto.getRegisterDate().getTime())); //Una fecha "util" no es una "sql" pero si al reves, por eso se debe hacer un cast
+      }
+      stmt.executeUpdate(); //Porque es una actualización
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
   public void delete(Long id) {
-
+    try( PreparedStatement stmt = getConnection().prepareStatement("DELETE FROM productos WHERE id=?") ) {
+      stmt.setLong(1, id);
+      stmt.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
